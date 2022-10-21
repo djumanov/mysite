0ebab4cb7778146f725f6954f0ec64f1dac1308c
djumanov <diyorbekjumanov1120@gmail.com>
2022-10-21 10:30:15 +0500

    create app

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,15 +1,8 @@
 
-from curses.ascii import HT
 from django.contrib import admin
 from django.urls import path
 
-from django.http import HttpResponse
-
-
-def home(r):
-    return HttpResponse('<h1>done</h1>')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
 ]
